57942621284c27f1490f9103d61d22aefd317510
luis.villarroel <ruif3rofficial@gmail.com>
2025-12-05 17:27:02 -0300

    unused import removed

--- mvi-common/src/main/kotlin/com/ruif3r/mvi-common/DefaultActivity.kt
+++ mvi-common/src/main/kotlin/com/ruif3r/mvi-common/DefaultActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.Lifecycle
